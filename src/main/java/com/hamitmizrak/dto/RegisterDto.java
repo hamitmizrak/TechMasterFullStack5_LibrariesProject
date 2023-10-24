3b1cfc1dbcd23d28281cee5660236dfe7ab8df0d
HamitMizrak <hamitmizrak@gmail.com>
2023-10-24 11:47:25 +0300

    RegisterDto Bean

--- src/main/java/com/hamitmizrak/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/dto/RegisterDto.java
@@ -1,7 +1,11 @@
 package com.hamitmizrak.dto;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Date;
+
+// RegisterDto
 public class RegisterDto extends  BaseDto implements Serializable {
 
     // Serileştirme
@@ -13,8 +17,57 @@ public class RegisterDto extends  BaseDto implements Serializable {
     private String uPassword;
 
     // Constructor
+    public RegisterDto() {
+    }
+
+    public RegisterDto(String uNickName, String uEmailAddress, String uPassword) {
+        this.uNickName = uNickName;
+        this.uEmailAddress = uEmailAddress;
+        this.uPassword = uPassword;
+    }
+
+    public RegisterDto(Long id, Date systemCreatedDate, String uNickName, String uEmailAddress, String uPassword) {
+        super(id, systemCreatedDate);
+        this.uNickName = uNickName;
+        this.uEmailAddress = uEmailAddress;
+        this.uPassword = uPassword;
+    }
 
     // toString
+    @Override
+    public String toString() {
+        return "RegisterDto{" +
+                "uNickName='" + uNickName + '\'' +
+                ", uEmailAddress='" + uEmailAddress + '\'' +
+                ", uPassword='" + uPassword + '\'' +
+                ", id=" + id +
+                ", systemCreatedDate=" + systemCreatedDate +
+                "} " + super.toString();
+    }
+
 
     //Getter And Setter
+    public String getuNickName() {
+        return uNickName;
+    }
+
+    public void setuNickName(String uNickName) {
+        this.uNickName = uNickName;
+    }
+
+    public String getuEmailAddress() {
+        return uEmailAddress;
+    }
+
+    public void setuEmailAddress(String uEmailAddress) {
+        this.uEmailAddress = uEmailAddress;
+    }
+
+    public String getuPassword() {
+        return uPassword;
+    }
+
+    public void setuPassword(String uPassword) {
+        this.uPassword = uPassword;
+    }
 } //end  class
