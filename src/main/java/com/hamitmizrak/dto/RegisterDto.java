099cdaf8daf863ff60d4872648252b4075e4401e
HamitMizrak <hamitmizrak@gmail.com>
2023-10-26 10:59:48 +0300

    SQL

--- src/main/java/com/hamitmizrak/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/dto/RegisterDto.java
@@ -13,24 +13,31 @@ public class RegisterDto extends  BaseDto implements Serializable {
     private String uEmailAddress;
     private String uPassword;
 
+    private int remainingNumber;
+    private Boolean isPassive;
+
     // Constructor (parametresiz)
     public RegisterDto() {
     }
 
     // Constructor (parametreli)
-    public RegisterDto(String uNickName, String uEmailAddress, String uPassword) {
+    public RegisterDto(String uNickName, String uEmailAddress, String uPassword,int remainingNumber,Boolean isPassive) {
         this.uNickName = uNickName;
         this.uEmailAddress = uEmailAddress;
         this.uPassword = uPassword;
+        this.remainingNumber = remainingNumber;
+        this.isPassive = isPassive;
     }
 
     // Constructor (parametreli)
     // Overloadinng
-    public RegisterDto(Long id, Date systemCreatedDate, String uNickName, String uEmailAddress, String uPassword) {
+    public RegisterDto(Long id, Date systemCreatedDate, String uNickName, String uEmailAddress, String uPassword,int remainingNumber,Boolean isPassive) {
         super(id, systemCreatedDate);
         this.uNickName = uNickName;
         this.uEmailAddress = uEmailAddress;
         this.uPassword = uPassword;
+        this.remainingNumber = remainingNumber;
+        this.isPassive = isPassive;
     }
 
     // toString
@@ -40,6 +47,8 @@ public class RegisterDto extends  BaseDto implements Serializable {
                 "uNickName='" + uNickName + '\'' +
                 ", uEmailAddress='" + uEmailAddress + '\'' +
                 ", uPassword='" + uPassword + '\'' +
+                ", remainingNumber='" + remainingNumber + '\'' +
+                ", isPassive='" + isPassive + '\'' +
                 ", id=" + id +
                 ", systemCreatedDate=" + systemCreatedDate +
                 "} " + super.toString();
@@ -68,5 +77,21 @@ public class RegisterDto extends  BaseDto implements Serializable {
 
     public void setuPassword(String uPassword) {
         this.uPassword = uPassword;
+    }
+
+    public int getRemainingNumber() {
+        return remainingNumber;
+    }
+
+    public void setRemainingNumber(int remainingNumber) {
+        this.remainingNumber = remainingNumber;
+    }
+
+    public Boolean getPassive() {
+        return isPassive;
+    }
+
+    public void setPassive(Boolean passive) {
+        isPassive = passive;
     }
 } //end  class
