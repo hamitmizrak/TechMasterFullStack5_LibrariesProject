0fdeedb52794b883dc7d24b921b8c504856dd3a5
HamitMizrak <hamitmizrak@gmail.com>
2023-11-02 10:15:36 +0300

    Mysql Event

--- src/main/java/com/hamitmizrak/database/Sql_tutorials.sql
+++ src/main/java/com/hamitmizrak/database/Sql_tutorials.sql
@@ -1,57 +1,76 @@
--- Create Database
-CREATE SCHEMA `libraries` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
-
 -- DELETE DATABASE
-DROP DATABASE `libraries`;
+DROP DATABASE `cars`;
+
+-- Create Database
+CREATE SCHEMA `cars` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
 
 -- Set
-use libraries;
+use cars;
 
 -- Table
-CREATE TABLE `libraries`.`register` (
+CREATE TABLE `cars`.`register` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nick_name` VARCHAR(255) NULL DEFAULT 'takma adınıız yazmadınız.',
   `email_address` VARCHAR(255) NULL DEFAULT 'email adres yazmadınız',
   `password` VARCHAR(45) NULL DEFAULT 'şifrenizi girmediniz',
-  `roles` VARCHAR(5) NULL DEFAULT 'user',
+  `roles` VARCHAR(10) NULL DEFAULT 'user',
   `remaining_number` INT(2) NULL DEFAULT '4',
   `is_passive` VARCHAR(2) NULL DEFAULT '0',
   PRIMARY KEY (`id`))
   ENGINE = InnoDB;
 
 -- tabloyayeni kolon eklemek
-ALTER TABLE `libraries`.`register` 
-ADD COLUMN `sytem_created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `is_passive`;
+ALTER TABLE `cars`.`register`
+ADD COLUMN `system_created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `is_passive`;
+
+-- Rolles
+ALTER TABLE `cars`.`register`
+CHANGE COLUMN `roles` `roles` VARCHAR(10) NULL DEFAULT 'user' ;
+
 
 -- DELETE TABLE
-DROP TABLE `libraries`.`register`;
+DROP TABLE `cars`.`register`;
 
 -- TRUNCATE Table
-TRUNCATE `libraries`.`register`;
+TRUNCATE `cars`.`register`;
 
 -- INSERT
-INSERT INTO `libraries`.`register` (`nick_name`, `email_address`, `password`, `roles`, `remaining_number`, `is_passive`) VALUES ('computer', 'hamitmizrak@gmail.com', '123456','user','5','0');
+-- insert into database.table () values ()
+INSERT INTO `cars`.`register` (`nick_name`, `email_address`, `password`, `roles`, `remaining_number`, `is_passive`) VALUES ('computer', 'hamitmizrak@gmail.com', '123456','admin','5','1');
 
 -- FIND
-SELECT * FROM libraries.register where id=1;
+SELECT * FROM cars.register where id=1;
+SELECT * FROM cars.register where email_address='eroot'
 
 -- UPDATE
-UPDATE `libraries`.`register` SET `nick_name`="nick", `email_address`="email@gmail.com", `password`="123456", `roles`="user", `remaining_number`=5, `is_passive`=0 WHERE (`id` = '1');
-
-UPDATE `libraries`.`register` SET  `remaining_number`=5 WHERE `email_address` = 'email@gmail.com';
+-- update database.table  set attributes="içerik"
+UPDATE `cars`.`register` SET `nick_name`="nick", `email_address`="email@gmail.com", `password`="123456", `roles`="user", `remaining_number`=5, `is_passive`=1 WHERE (`id` = '1');
 
 -- DELETE
-DELETE FROM `libraries`.`register` WHERE (`id` = '1');
+DELETE FROM `cars`.`register` WHERE (`id` = '1');
 
 -- SELECT
-SELECT * FROM libraries.register;
-SELECT * FROM libraries.register where email_address="hamitmizrak";
-select count(*) from `libraries`.`register`;
+SELECT * FROM cars.register;
+SELECT * FROM cars.register where email_address="hamitmizrak";
+select count(*) from `cars`.`register`;
 
 -- LIKE --
-select * from `libraries`.`register`;
-select * from `libraries`.`register` where id=2;
-select * from `libraries`.`register` where nick_name like 'c%'; -- c harfiyle başladı
-select * from `libraries`.`register` where nick_name like 'b%' or email_address like 'da%';
-select * from `libraries`.`register` where nick_name like '%4'; -- 4 ile bitti
-select * from `libraries`.`register` where nick_name like '_o%'; -- ikinci karakter o olanlar
+select * from `cars`.`register`;
+select * from `cars`.`register` where id=2;
+select * from `cars`.`register` where nick_name like 'c%'; -- c harfiyle başladı
+select * from `cars`.`register` where nick_name like 'b%' or email_address like 'da%';
+select * from `cars`.`register` where nick_name like '%4'; -- 4 ile bitti
+select * from `cars`.`register` where nick_name like '_o%'; -- ikinci karakter o olanlar
+
+-- EVENTS
+SELECT * FROM cars.register;
+use cars;
+show events from  cars;
+drop event on_time user_is_passive
+
+
+CREATE EVENT IF NOT EXISTS  user_is_passive
+ON schedule at current_timestamp() + interval 1 minute
+on completion preserve
+do
+  UPDATE `cars`.`register` SET `is_passive`=0 WHERE `id`='3';
